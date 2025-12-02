99601ab8c35b1e9b65a94e9b9d66bb42fb4f6a50
banna <goutamsinghpatel2005@gmail.com>
2025-12-02 19:00:34 +0530

    last

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -51,10 +51,9 @@ export const login=async(req,res)=>{
          res.cookie('token',token,
             {
                   httpOnly:true,
-                //   local
-        // secure:process.env.NODE_ENV === "production" ,
-        // sameSite:process.env.NODE_ENV==='production'?'none':'strict',
-        // web-site
+              
+      
+        
          secure:true,
     sameSite:'none',
         maxAge:2*24*60*60*1000,
@@ -76,9 +75,7 @@ export  const logout=async(req,res)=>{
 try {
       res.clearCookie("token", {
     httpOnly: true,
-    //  secure:process.env.NODE_ENV === "production" ,
-    //     sameSite:process.env.NODE_ENV==='production'?'none':'strict',
-        // web-site
+   
          secure:true,
     sameSite:'none',
   });
