68d2e49c2e1b50c399d269793130373b123a3eea
banna <goutamsinghpatel2005@gmail.com>
2025-12-02 18:59:54 +0530

    final

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -76,8 +76,11 @@ export  const logout=async(req,res)=>{
 try {
       res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    //  secure:process.env.NODE_ENV === "production" ,
+    //     sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+        // web-site
+         secure:true,
+    sameSite:'none',
   });
   return res.json({success:true,message:"logout"});
     
